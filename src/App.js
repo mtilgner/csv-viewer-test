4896ffc4022e0c984b62763eabe1be0317893b03
mm <martentilgner@gmail.com>
2022-09-04 14:10:21 +0200

    Geruest fuer Dummy-Table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import {useTable} from "react-table";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table />
     </div>
+  );
+}
+
+function Table(){
+  const data = React.useMemo(
+    () => [
+      {
+        sp1: "test1",
+        sp2: "xyz",
+      },
+      {
+        sp1: "test2",
+        sp2: "abc",
+      },
+    ],
+    []
+  );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Spalte1",
+        accessor: "sp1",
+      },
+      {
+        Header: "Spalte2",
+        accessor: "sp2",
+      },
+    ],
+    []
+  );
+
+  const tableInstance = useTable({columns, data})
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return(
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return(
+                  <td {...cell.getCellProps()}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
